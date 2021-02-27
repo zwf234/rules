059707500c1b9a552c06e907c121f7435df7b161
陌路 <48481993+zwf234@users.noreply.github.com>
2021-02-27 22:06:46 +0800

    Update xxysad.js

--- xxysad.js
+++ xxysad.js
@@ -1,15 +1,19 @@
 /*
-更新：2021.01.28
+更新：2021.02.27
 小小影视 无限观看&免广告
-QX:
-https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index) url script-response-body https://raw.githubusercontent.com/zwf234/rules/master/xxysad.js
-Surge:
-http-response https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/zwf234/rules/master/xxysad.js
-MITM = *.xxjjappss.com,*.huaerdadi.com
-*/
+电报频道：@qixinscience
+感谢@GB-png，提供新方法
 
+QX:
+https:\/\/.*\.(xiaoxiaoapps|xiaoxiaoimg)\.com\/(ssp-svr\/ssp\/list3|ucp/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/zwf234/rules/master/xxysad.js
+Surge:
+http-response https:\/\/.*\.(xiaoxiaoapps|xiaoxiaoimg)\.com\/(ssp-svr\/ssp\/list3|ucp/index|getGlobalData) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/zwf234/rules/master/xxysad.js
+
+MITM = *.xxjjappss.com,*.huaerdadi.com,*.xiaoxiaoapps.com,*.xiaoxiaoimg.com
+*/
 const path1 = "/ucp/index";
 const ad = 'ssp-svr/ssp/list3';
+const adold = 'getGlobalData';
 let obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(path1) != -1){
@@ -22,6 +26,12 @@ if ($request.url.indexOf(path1) != -1){
 
 if ($request.url.indexOf(ad) != -1) {
 delete obj.data.pmap
+}
+
+if ($request.url.indexOf(adold) != -1) {
+delete obj.data.adrows
+delete obj.data.adgroups
+delete obj.data.iOS_adgroups
 }
 
 $done({body: JSON.stringify(obj)});
